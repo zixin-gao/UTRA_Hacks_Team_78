4595da8f0101e724a15debeb7014f8f341edd4da
Richa <richashah015@gmail.com>
2026-01-31 21:34:16 -0500

    tested_arm_angle

--- servo_motor_testing/servo_motor_testing.ino
+++ servo_motor_testing/servo_motor_testing.ino
@@ -1,16 +1,17 @@
 #include <Servo.h> // Include the Servo library
 
 Servo myServo;     // Create a servo object to control the servo
-int angle = 0;     // Variable to store the servo angle
+int angle = 60;
 
 void setup() {
-  myServo.attach(9); // Attach the servo to pin 9
-  for (angle = 0; angle <= 45; angle += 1) { 
+  myServo.attach(11);
+  // myServo.write(angle);
+  for (angle = 60; angle > 40; angle -= 10) { 
     myServo.write(angle); // Set servo to angle
-    delay(15);            // Wait for the servo to reach the position
+    delay(10000);            // Wait for the servo to reach the position
+    Serial.println(angle);
   }
 }
 
 void loop() {
-  // Sweep from 0 to 180 degrees
 }
